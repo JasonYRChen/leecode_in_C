15ceb7e540509c22ec06fa7baa6b0ae5c25ea49b
JasonYRChen <jkmylife@gmail.com>
2022-03-17 14:44:44 +0800

    9. Palindrome Number

---
+++ Palindrome_Number_9.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int isPalindrome(int x){
+    if (x < 0)
+        return 0;
+    if (x == 0)
+        return 1;
+    int digits = 0;
+    int y = x;
+    while (y) {
+        y = y / 10;
+        digits++;
+    } 
+
+    y = x;
+    int numbers[digits];
+    for (int i=0; i<digits; i++) {
+        numbers[i] = y % 10;
+        y = y / 10;
+    }
+
+    int left = 0;
+    int right = digits - 1;
+    while (left < right) {
+        if (numbers[left] != numbers[right])
+            return 0;
+        left++;
+        right--;
+    }
+    return 1;
+}
+
+int main()
+{
+    int x = 1;
+    
+    printf("IsPalindrome: %i\n", isPalindrome(x));
+
+    return 0;
+}
+
